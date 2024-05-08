28b61af139b3b98bf2a6d32b3dcc50cbef0d98a4
Aman Tiwari <amantiwari8861@gmail.com>
2024-05-08 15:48:32 +0530

    js basics completed

---
+++ temp/Hello2.js
@@ -0,0 +1,21 @@
+// console.log("hello world");
+
+// let myFunction=function volOfSphere(radius){ return 4/3*Math.PI*radius**3; }
+// let myFunction=function(radius){ return 4/3*Math.PI*radius**3; }
+// let myFunction=(radius)=>{ return 4/3*Math.PI*radius**3; }
+// let myFunction=(radius)=> 4/3*Math.PI*radius**3;
+
+// console.log(volOfSphere(2.5));
+// console.log(myFunction(2.5));
+
+
+// function printSkillName(skill) 
+// {
+//     console.log(skill);
+// }
+// skills.forEach(printSkillName);
+
+// let skills=["C","C++","Java","Python","etc"];
+// skills.forEach(skill=>console.log(skill));
+// ["C","C++","Java","Python","etc"].forEach(skill=>console.log(skill));
+
