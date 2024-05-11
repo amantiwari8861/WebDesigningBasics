f61540bbad00c6c385aa582641afac6e390742c9
Aman Tiwari <amantiwari8861@gmail.com>
2024-05-11 12:53:19 +0530

    import export

---
+++ 03.JS/29.importexport/File1.js
@@ -0,0 +1,10 @@
+let a=10;
+let b=20;
+
+function add(a,b){
+    return a+b;
+}
+
+
+export default a;
+// export {a,b,add}
---
+++ 03.JS/29.importexport/File2.js
@@ -0,0 +1,15 @@
+// import a from './File1.js'
+// import {a,b} from './File1.js'
+// import {a,b,add} from './File1.js'
+
+// console.log(a);
+// console.log(b);
+// console.log(add(a,b));
+
+// import {a as myvariable} from './File1.js'
+
+// console.log(myvariable);
+
+import xyz from './File1.js'
+
+console.log(xyz);
